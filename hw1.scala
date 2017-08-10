c841f89a41ec5ded152fc344d149ac02d6b514f1
Chris Okasaki <Chris.Okasaki@usma.edu>
2017-08-10 13:55:10 -0400

    HW1 starter code

---
+++ hw1.scala
@@ -0,0 +1,13 @@
+object hw1 extends eecs.cs478 {
+  def userName = ???
+
+  def sumArray(a: Array[Int]): Int = {
+    ???
+  }
+  test("sumArray", sumArray _, "a")
+
+  def sumList(list: List[Int]): Int = {
+    ???
+  }
+  ignoretest("sumList", sumList _, "list")
+}
