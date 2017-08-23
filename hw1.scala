a158295f4fa0cd171c44ebebc33f4bebfc2eb77b
DekuZero <conner.cj.russell@gmail.com>
2017-08-22 23:45:24 -0400

    Turnin

--- hw1.scala
+++ hw1.scala
@@ -1,20 +1,23 @@
 object hw1 extends eecs.cs478 {
-  def userName = "DekuZero"
+  def userName = "DekuZero (Conner Russell)"
 
   def sumArray(a: Array[Int]): Int = {
     var numSum = 0
-    for (n <- a.length){
-      numSum += n
+    for (i <- 0 until a.length) {
+      numSum += a(i)
     }
+    return numSum
   }
   test("sumArray", sumArray _, "a")
 
   def sumList(list: List[Int]): Int = {
     var numSum = 0
-    while (list.isNotEmpty){
-      numSum += list.head
-      list = list.tail
+    var nlist = list
+    while (!nlist.isEmpty) {
+      numSum += nlist.head
+      nlist = nlist.tail
     }
+    return numSum
   }
   test("sumList", sumList _, "list")
 }
