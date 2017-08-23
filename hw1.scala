ae6b4269621fb6b2c99db2959c022f9e0c73bf5b
DekuZero <conner.cj.russell@gmail.com>
2017-08-22 22:36:17 -0400

    Completed problems #1 & #2

--- hw1.scala
+++ hw1.scala
@@ -1,13 +1,20 @@
 object hw1 extends eecs.cs478 {
-  def userName = ???
+  def userName = "DekuZero"
 
   def sumArray(a: Array[Int]): Int = {
-    ???
+    var numSum = 0
+    for (n <- a.length){
+      numSum += n
+    }
   }
   test("sumArray", sumArray _, "a")
 
   def sumList(list: List[Int]): Int = {
-    ???
+    var numSum = 0
+    while (list.isNotEmpty){
+      numSum += list.head
+      list = list.tail
+    }
   }
-  ignoretest("sumList", sumList _, "list")
+  test("sumList", sumList _, "list")
 }
